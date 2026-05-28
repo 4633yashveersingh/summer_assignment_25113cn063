a98fff88dab9e1f8c2692eee500ad156966168c2
4633yashveersingh <4633yashveersingh10d@gmail.com>
2026-05-28 23:44:09 +0530

    Add files via upload

---
+++ coutn_digit.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num,count=0;
+    cout<<"Enter no to count digit"<<endl;
+    cin>>num;
+    int temp = num;
+    while(num>0)
+    {
+        num=num/10;
+        count++;
+    }
+    cout<<"the no of digits in "<< temp << "is" << count<<endl;
+    return 0;
+}
